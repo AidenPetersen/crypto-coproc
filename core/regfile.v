223c23620b2bcfee46007be347aa76c25da3c31b
Aiden Petersen <wdaidenp@iastate.edu>
2022-08-17 09:27:15 -0500

    Added regfile

---
+++ core/regfile.v
@@ -0,0 +1,33 @@
+module regfile (
+    // inputs
+    input clk,
+    input rst,
+
+    input write_enable,
+    input [4:0] write_addr,
+    input [31:0] write_data,
+
+    input [4:0] read0_addr,
+    input [4:0] read1_addr,
+
+    // outputs
+    output reg [31:0] read0_data,
+    output reg [31:0] read1_data
+);
+    reg [31:0] registers [31:0];
+
+    always @(posedge clk) begin
+        if(write_enable) 
+            registers[write_addr] <= write_data;
+    end
+
+    integer i;
+    always @(*) begin
+        if(rst) begin
+            for(i = 0; i < 32; i = i + 1) registers[i] <= 32'h0000;
+        end
+        registers[0] <= 0;
+        read0_data <= registers[read0_addr];
+        read1_data <= registers[read1_addr];
+    end
+endmodule
---
+++ tb/regfile_tb.v
@@ -0,0 +1,52 @@
+`timescale 1ns/1ps
+module regfile_tb;
+    reg clk, rst, write_enable;
+    reg [4:0] write_addr, read0_addr, read1_addr;
+    reg [31:0] write_data;
+    wire [31:0] read0_data, read1_data;
+    
+    regfile dut (
+        .clk(clk),
+        .rst(rst),
+        
+        .write_enable(write_enable),
+
+        .write_addr(write_addr),
+        .write_data(write_data),
+
+        .read0_addr(read0_addr),
+        .read1_addr(read1_addr),
+
+        .read0_data(read0_data),
+        .read1_data(read1_data)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        #7.5 rst <= 1;
+        #5 rst <= 0;
+        write_enable <= 1;
+        write_data <= 15;
+        write_addr <= 15;
+        read0_addr <= 15;
+        read1_addr <= 0;
+        #10
+        write_data <= 123;
+        write_addr <= 0;
+        #10
+        write_data <= 123;
+        write_addr <= 1;
+        read1_addr <= 1;
+        #10
+        $finish;
+    end
+
+
+    initial begin
+        $dumpfile("regfile_tb.vcd");
+        $dumpvars(0, regfile_tb);
+    end
+
+endmodule
